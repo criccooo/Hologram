48aaa8f19e5b94ed36b2d650dc22e99552d81fc1
criccooo <159587365+criccooo@users.noreply.github.com>
2026-03-27 11:23:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const playerCamera = document.getElementById('player-camera');
 const debugText = document.getElementById('debug-text');
 
 // --- CONNESSIONE SERVER PUBBLICO ---
-const mqttClient = mqtt.connect('wss://broker.hivemq.com:8000/mqtt');
+const mqttClient = mqtt.connect('wss://broker.emqx.io:8084/mqtt');
 const topicSegreto = 'esame_ar_visore_2026_super_segreto'; // DEVE ESSERE IDENTICO SUL PC
 
 mqttClient.on('connect', () => {
